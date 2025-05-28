01ef0b6c61e1001a37d6668029d67649a281790b
Deivid Agudelo Corzo <132175069+D31V1X@users.noreply.github.com>
2025-05-27 23:37:58 -0500

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from Flask import Flask, render_template, request
+from flask import Flask, render_template, request
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import os
