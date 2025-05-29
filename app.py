c3fb237db7f59e40205d192f3efcda0d692f72e4
Deivid Agudelo Corzo <132175069+D31V1X@users.noreply.github.com>
2025-05-29 04:23:19 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import re
 from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
-app.secret_key = 'tu_clave_secreta_aqui'  # Cambia esto por una clave secreta segura
+app.secret_key = 'ucentral'  # Cambia esto por una clave secreta segura
 
 # Agregar la función now al contexto de la plantilla
 @app.context_processor
@@ -17,13 +17,13 @@ def inject_now():
     return {'now': datetime.now}
 
 # Versión de la aplicación
-VERSION_APP = "Versión 2.2 del Mayo 22 del 2025"
-CREATOR_APP = "Nombre del creador/ruta github"
+VERSION_APP = "Versión 1 del 29 Mayo del 2025"
+CREATOR_APP = "https://github.com/D31V1X/BigDataApp/tree/BigDataApp"
 mongo_uri   = os.environ.get("MONGO_URI")
 
 if not mongo_uri:
     #uri = "mongodb+srv://DbCentral:DbCentral2025@cluster0.vhltza7.mongodb.net/?appName=Cluster0"
-    uri         = "mongodb+srv://DbCentral:DbCentral2025@cluster0.vhltza7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    uri         = "mongodb+srv://DbCentral:DbCentral@cluster0.gqd4gxq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     mongo_uri   = uri
 
 # Función para conectar a MongoDB
